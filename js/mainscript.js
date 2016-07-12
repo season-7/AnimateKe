a89ed44a4d95038384fd331fff448d333a8cdbab
Brian Lusina <lusinabrian@gmail.com>
2016-07-12 16:01:55 +0300

    Add animation array

--- js/mainscript.js
+++ js/mainscript.js
@@ -235,7 +235,14 @@ $(document).ready(function () {
 	
 	
 	/*animate button*/
-	var animationsArr = ['',];
+	/*animations array to loop through at random*/
+	var animationsArr = ['jello', 'bounce', 'flash', 'pulse', 'pulse', 'rubberBand', 'shake', 'headShake', 'swing', 'tada', 'wobble', 'jello', 'bounceIn', 'bounceInDown', 'bounceInLeft', 'bounceInRight', 'bounceInUp', 'bounceOut', 'bounceOutDown', 'bounceOutLeft', 'bounceOutRight', 'bounceOutUp', 'fadeIn', 'fadeIn', 'fadeInDown', 'fadeInDownBig',
+'fadeInLeft', 'fadeInLeftBig', 'fadeInRight', 'fadeInRightBig', 'fadeInUp', 'fadeInUpBig', 'fadeOut', 'fadeOutDown','fadeOutDownBig'
+,'fadeOutLeft','fadeOutLeftBig','fadeOutRight','fadeOutRightBig','fadeOutUp','fadeOutUpBig','flipInX','flipInY','flipOutX','flipOutY','lightSpeedIn','lightSpeedOut','rotateIn','rotateInDownLeft','rotateInDownRight','rotateInUpLeft','rotateInUpRight','rotateOut','rotateOutDownLeft','rotateOutDownRight','rotateOutUpLeft','rotateOutUpRight'
+,'hinge','rollIn','rollOut','zoomIn','zoomInDown','zoomInLeft','zoomInRight','zoomInUp','zoomOut','zoomOutDown'
+,'zoomOutLeft','zoomOutRight','zoomOutUp','slideInDown'
+,'slideInLeft','slideInRight','slideInUp','slideOutDown'
+,'slideOutLeft','slideOutRight','slideOutUp'];
 	$('#call-animate-btn').on('click',function(){
 		$(this).find('a').toggleClass('jello');
 	});/*end animate*/
