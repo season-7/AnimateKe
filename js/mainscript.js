66d5d5d4d381a0ba712bd417afcf2a0ddba4426e
Brian Lusina <lusinabrian@gmail.com>
2016-07-12 16:40:01 +0300

    Add animations to animate button

--- js/mainscript.js
+++ js/mainscript.js
@@ -235,16 +235,18 @@ $(document).ready(function () {
 	
 	
 	/*animate button*/
-	/*animations array to loop through at random*/
+	/*animations array to loop through each animation at random when the user clicks the animate button*/
 	var animationsArr = ['jello', 'bounce', 'flash', 'pulse', 'pulse', 'rubberBand', 'shake', 'headShake', 'swing', 'tada', 'wobble', 'jello', 'bounceIn', 'bounceInDown', 'bounceInLeft', 'bounceInRight', 'bounceInUp', 'bounceOut', 'bounceOutDown', 'bounceOutLeft', 'bounceOutRight', 'bounceOutUp', 'fadeIn', 'fadeIn', 'fadeInDown', 'fadeInDownBig',
-'fadeInLeft', 'fadeInLeftBig', 'fadeInRight', 'fadeInRightBig', 'fadeInUp', 'fadeInUpBig', 'fadeOut', 'fadeOutDown','fadeOutDownBig'
-,'fadeOutLeft','fadeOutLeftBig','fadeOutRight','fadeOutRightBig','fadeOutUp','fadeOutUpBig','flipInX','flipInY','flipOutX','flipOutY','lightSpeedIn','lightSpeedOut','rotateIn','rotateInDownLeft','rotateInDownRight','rotateInUpLeft','rotateInUpRight','rotateOut','rotateOutDownLeft','rotateOutDownRight','rotateOutUpLeft','rotateOutUpRight'
-,'hinge','rollIn','rollOut','zoomIn','zoomInDown','zoomInLeft','zoomInRight','zoomInUp','zoomOut','zoomOutDown'
-,'zoomOutLeft','zoomOutRight','zoomOutUp','slideInDown'
-,'slideInLeft','slideInRight','slideInUp','slideOutDown'
-,'slideOutLeft','slideOutRight','slideOutUp'];
+'fadeInLeft', 'fadeInLeftBig', 'fadeInRight', 'fadeInRightBig', 'fadeInUp', 'fadeInUpBig', 'fadeOut', 'fadeOutDown','fadeOutDownBig','fadeOutLeft','fadeOutLeftBig','fadeOutRight','fadeOutRightBig','fadeOutUp','fadeOutUpBig','flipInX','flipInY','flipOutX','flipOutY','lightSpeedIn','lightSpeedOut','rotateIn','rotateInDownLeft','rotateInDownRight','rotateInUpLeft','rotateInUpRight','rotateOut','rotateOutDownLeft','rotateOutDownRight','rotateOutUpLeft','rotateOutUpRight','hinge','rollIn','rollOut','zoomIn','zoomInDown','zoomInLeft','zoomInRight','zoomInUp','zoomOut','zoomOutDown','zoomOutLeft','zoomOutRight','zoomOutUp','slideInDown','slideInLeft','slideInRight','slideInUp','slideOutDown','slideOutLeft','slideOutRight','slideOutUp'];
+	
+	var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	
 	$('#call-animate-btn').on('click',function(){
-		$(this).find('a').toggleClass('jello');
+
+		var currentAnim = animationsArr[Math.floor(Math.random()*animationsArr.length)];
+		
+		$(this).find('a').addClass(currentAnim).one(animationEnd,function(){
+			$(this).removeClass(currentAnim);});
 	});/*end animate*/
 	
 	});
